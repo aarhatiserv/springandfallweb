2e200bd099faaa821279730fbf92c5da73d014b7
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-11-26 18:02:55 +0530

    Spring And Fall Nazim 26 mod Nov

--- public/assets/js/career-guide.js
+++ public/assets/js/career-guide.js
@@ -697,7 +697,7 @@ function addPersonalDetailsInSession(
 
 // #############################################Start For Calls Country Courses And Levels FUnctions #################################
 $(document).ready(function () {
-
+alert('country');
   // ------------------------------------Country--------------------
   $("input:radio[name=country]").change(function () {
 
