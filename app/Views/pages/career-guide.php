f55dec67418a14a8cd2760ddbe63d95d7bdcdd1c
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-02 12:05:40 +0530

    Spring And Fall Nazim update career-guide updated new10

--- app/Views/pages/career-guide.php
+++ app/Views/pages/career-guide.php
@@ -3,12 +3,13 @@
 <!-- added by debanjan | 28-jul-2021 -->
 <!--Start of Jumbotron-->
 <!-- here .. i have show you how to export image from figma..just link here start  -->
-
-//  if($session->get('token')){
-   <script type="text/javascript">
-    $(document).ready(function(){ alert("asvae") });
-   </script>
-
+<?php if($session->get('token')){?>
+<style type="text/css">
+#areaOfInterest {
+    display: block;
+}
+</style>
+<?php }?>
 <section id="getstartdSection">
     <div class="container">
         <div class="row">
@@ -33,65 +34,65 @@
 
 <section id="personalDetails" style="display: none;">
 
-<!-- First Name alert -->
-<div class="alert" id="alertFirstName" style="display:none">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
-  First Name required.
-</div>
-<!-- End alert -->
+    <!-- First Name alert -->
+    <div class="alert" id="alertFirstName" style="display:none">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        First Name required.
+    </div>
+    <!-- End alert -->
 
-<!-- Phone Number alert -->
-<div class="alert" id="alertPhone" style="display:none">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
-  Phone Number required.
-</div>
-<!-- End alert -->
+    <!-- Phone Number alert -->
+    <div class="alert" id="alertPhone" style="display:none">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        Phone Number required.
+    </div>
+    <!-- End alert -->
 
-<!-- Email alert -->
-<div class="alert" id="alertEmail" style="display:none">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
-  Email required.
-</div>
-<!-- End alert -->
+    <!-- Email alert -->
+    <div class="alert" id="alertEmail" style="display:none">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        Email required.
+    </div>
+    <!-- End alert -->
 
-<!-- City alert -->
-<div class="alert" id="alertCity" style="display:none">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
-  City required.
-</div>
-<!-- End alert -->
+    <!-- City alert -->
+    <div class="alert" id="alertCity" style="display:none">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        City required.
+    </div>
+    <!-- End alert -->
 
-<!-- State alert -->
-<div class="alert" id="alertState" style="display:none">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
-  State required.
-</div>
-<!-- End alert -->
+    <!-- State alert -->
+    <div class="alert" id="alertState" style="display:none">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        State required.
+    </div>
+    <!-- End alert -->
 
-<!-- Pin alert -->
-<div class="alert" id="alertPin" style="display:none">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
-  Pin Code required.
-</div>
-<!-- End alert -->
+    <!-- Pin alert -->
+    <div class="alert" id="alertPin" style="display:none">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        Pin Code required.
+    </div>
+    <!-- End alert -->
 
-<!-- Email Validationin alert -->
-<div class="alert" id="alertEmailValidation" style="display:none">
-  <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span> 
-  You have entered an invalid email address!
-</div>
-<!-- End alert -->
+    <!-- Email Validationin alert -->
+    <div class="alert" id="alertEmailValidation" style="display:none">
+        <span class="closebtn" onclick="this.parentElement.style.display='none';">&times;</span>
+        You have entered an invalid email address!
+    </div>
+    <!-- End alert -->
     <div class="container">
         <div class="row">
             <h5 class="pl-1 pb-2" style="margin-bottom: 20px;">Personal details</h5>
         </div>
         <div class="row">
             <div class="col-lg-12">
-              <form enctype="multipart/form-data" style="width: 100%;">
-                <!-- image -->
-                <div class="form-row">
+                <form enctype="multipart/form-data" style="width: 100%;">
+                    <!-- image -->
+                    <div class="form-row">
 
-                    <!-- <div class="col mb-3 mx-2">
+                        <!-- <div class="col mb-3 mx-2">
                         <div class="file-inputCareer">
 
                             <input type="file" id="file" name="file" class="fileCareer" multiple required onchange="carrerGuideFileUpload(this)"/>
@@ -105,96 +106,101 @@
                         <div class="file-nameCareer">
                         </div>
                     </div> -->
-                </div>
-                <!-- image -->
+                    </div>
+                    <!-- image -->
 
 
-                <div class="form-row">
-                    <!-- <div class="col-md-6 mb-3 mx-2"> -->
-                    <div class="col mb-3 mx-2">
-                        <input type="text" class="form-control" name="careerFname" id="careerFname" placeholder="First Name required*">
+                    <div class="form-row">
+                        <!-- <div class="col-md-6 mb-3 mx-2"> -->
+                        <div class="col mb-3 mx-2">
+                            <input type="text" class="form-control" name="careerFname" id="careerFname"
+                                placeholder="First Name required*">
+                        </div>
+                        <!-- <div class="col-md-6 mb-3 mx-2"> -->
+                        <div class="col mb-3 mx-2">
+                            <input type="text" class="form-control" name="careerLname" id="careerLname"
+                                placeholder="Last Name required*">
+                        </div>
                     </div>
-                    <!-- <div class="col-md-6 mb-3 mx-2"> -->
-                    <div class="col mb-3 mx-2">
-                        <input type="text" class="form-control" name="careerLname" id="careerLname" placeholder="Last Name required*">
+                    <div class="form-row">
+                        <div class="col mb-3 mx-2">
+                            <input type="text" class="form-control" name="careerPhone" id="careerPhone" maxlength="10"
+                                placeholder="Phone Number required*" onkeypress="javascript:return isNumber(event)">
+                        </div>
+                        <div class="col mb-3 mx-2">
+                            <input type="email" class="form-control" name="careerEmail" id="careerEmail"
+                                placeholder="Email required*">
+                        </div>
                     </div>
-                </div>
-                <div class="form-row">
-                    <div class="col mb-3 mx-2">
-                        <input type="text" class="form-control" name="careerPhone" id="careerPhone" maxlength="10" placeholder="Phone Number required*"
-                        onkeypress="javascript:return isNumber(event)" >
+                    <div class="form-row">
+                        <div class="col mb-3 mx-2">
+                            <input type="text" class="form-control" name="careerAddressline1" id="careerAddressline1"
+                                placeholder="Address Line1">
+                        </div>
+                        <div class="col mb-3 mx-2">
+                            <input type="address" class="form-control" name="careerAddressline2" id="careerAddressline2"
+                                placeholder="Address Line2">
+                        </div>
                     </div>
-                    <div class="col mb-3 mx-2">
-                        <input type="email" class="form-control" name="careerEmail" id="careerEmail" placeholder="Email required*">
+                    <div class="form-row">
+                        <div class="col mb-3 mx-2">
+                            <input type="text" class="form-control" name="careerCity" id="careerCity"
+                                placeholder="City/Town/Village required*">
+                        </div>
+                        <div class="col mb-3 mx-2">
+                            <input type="text" class="form-control" name="careerState" id="careerState"
+                                placeholder="State required*">
+                        </div>
+                        <div class="col mb-3 mx-2">
+                            <input type="text" class="form-control" name="careerPin" id="careerPin" maxlength="6"
+                                placeholder="Pincode required*" onkeypress="javascript:return isNumber(event)">
+                        </div>
                     </div>
-                </div>
-                <div class="form-row">
-                    <div class="col mb-3 mx-2">
-                        <input type="text" class="form-control" name="careerAddressline1" id="careerAddressline1"
-                            placeholder="Address Line1">
-                    </div>
-                    <div class="col mb-3 mx-2">
-                        <input type="address" class="form-control" name="careerAddressline2" id="careerAddressline2"
-                            placeholder="Address Line2">
-                    </div>
-                </div>
-                <div class="form-row">
-                    <div class="col mb-3 mx-2">
-                        <input type="text" class="form-control" name="careerCity" id="careerCity" placeholder="City/Town/Village required*">
-                    </div>
-                    <div class="col mb-3 mx-2">
-                        <input type="text" class="form-control" name="careerState" id="careerState" placeholder="State required*">
-                    </div>
-                    <div class="col mb-3 mx-2">
-                        <input type="text" class="form-control" name="careerPin" id="careerPin" maxlength="6" placeholder="Pincode required*"
-                        onkeypress="javascript:return isNumber(event)">
-                    </div>
-                </div>
-</form>
+                </form>
                 <div class="row">
                     <h5 class="pl-1 pb-3" style="padding-bottom:20px;padding-top:10px">Qualification</h5>
                 </div>
                 <form style="width: 100%;">
-                <div class="row">
-                    <div class="col-lg-12">
+                    <div class="row">
+                        <div class="col-lg-12">
 
-                        <div class="form-row">
-                            <!-- <div class="col-md-6 mb-3 mx-2"> -->
-                            <div class="col mb-3 mx-2">
-                                <input type="text" class="form-control" name="careerQualification" id="careerQualification"
-                                    placeholder="Highest Qualification required*" >
+                            <div class="form-row">
+                                <!-- <div class="col-md-6 mb-3 mx-2"> -->
+                                <div class="col mb-3 mx-2">
+                                    <input type="text" class="form-control" name="careerQualification"
+                                        id="careerQualification" placeholder="Highest Qualification required*">
+                                </div>
+                                <!-- <div class="col-md-6 mb-3 mx-2"> -->
+                                <div class="col mb-3 mx-2">
+                                    <input type="text" class="form-control" name="careerPassingYear"
+                                        id="careerPassingYear" placeholder="Year of Passing required*">
+                                </div>
                             </div>
-                            <!-- <div class="col-md-6 mb-3 mx-2"> -->
-                            <div class="col mb-3 mx-2">
-                                <input type="text" class="form-control" name="careerPassingYear" id="careerPassingYear"
-                                    placeholder="Year of Passing required*">
+                            <div class="form-row">
+                                <div class="col mb-3 mx-2">
+                                    <input type="text" class="form-control" name="careerHigherSecondary"
+                                        id="careerHigherSecondary" placeholder="12th">
+                                </div>
+                                <div class="col mb-3 mx-2">
+                                    <input type="text" class="form-control" name="careerHigherSecondaryYear"
+                                        id="careerHigherSecondaryYear" placeholder="Year of Passing">
+                                </div>
                             </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="col mb-3 mx-2">
-                                <input type="text" class="form-control" name="careerHigherSecondary" id="careerHigherSecondary"
-                                    placeholder="12th">
+                            <div class="form-row">
+                                <div class="col mb-3 mx-2">
+                                    <input type="text" class="form-control" name="careerSecondary" id="careerSecondary"
+                                        placeholder="10th">
+                                </div>
+                                <div class="col mb-3 mx-2">
+                                    <input type="email" class="form-control" name="careerSecondaryPassingYear"
+                                        id="careerSecondaryPassingYear" placeholder="Year of Passing">
+                                </div>
                             </div>
-                            <div class="col mb-3 mx-2">
-                                <input type="text" class="form-control" name="careerHigherSecondaryYear"
-                                    id="careerHigherSecondaryYear" placeholder="Year of Passing">
-                            </div>
-                        </div>
-                        <div class="form-row">
-                            <div class="col mb-3 mx-2">
-                                <input type="text" class="form-control" name="careerSecondary" id="careerSecondary"
-                                    placeholder="10th">
-                            </div>
-                            <div class="col mb-3 mx-2">
-                                <input type="email" class="form-control" name="careerSecondaryPassingYear"
-                                    id="careerSecondaryPassingYear" placeholder="Year of Passing">
-                            </div>
+
                         </div>
 
                     </div>
-
-                </div>
-</form>
+                </form>
                 <div class="row p-1 float-right">
                     <button class="btn btn-primary font-weight-bold mb-2 mr-2" name="next" id="next"
                         type="submit">Next</button>
