5c7b08f695a86d7170055905b1912a0a599f6e8c
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-02 12:08:06 +0530

    Spring And Fall Nazim update career-guide updated new11

--- app/Controllers/User.php
+++ app/Controllers/User.php
@@ -279,7 +279,7 @@ class User extends ResourceController
 
 		$session = session();
 		$session->destroy();
-		return redirect()->to('/home');
+		return redirect()->to('https://spirngandfall.in/home');
 	}
 
 
