b64d38f95a474d8b49db87b73c8a7ce2075597d5
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-04 12:45:13 +0530

    Spring And Fall Nazim consultation apply 9

--- app/Controllers/CollegeController.php
+++ app/Controllers/CollegeController.php
@@ -330,7 +330,7 @@ class CollegeController extends BaseController
             //     echo json_encode(["status" => 2, "message" => "Your Query Submitted, but mail not send"]);
                 
             // }
-            echo json_encode(["status" => 1, "message" => "Your Query submitted, We'll callback soon.!!".$leadsData[0]['email']]);
+            echo json_encode(["status" => 1, "message" => "Your Query submitted, We'll callback soon.!!".$leadsData['spring_users.email']]);
            
         } else {
 
