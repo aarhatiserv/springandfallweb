8c6229c8e0bb29ca302414ce171aa395b59df773
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-07 13:30:19 +0530

    Spring And Fall Nazim email updated 7

--- app/Views/templates/emailForApplyColleges.php
+++ app/Views/templates/emailForApplyColleges.php
@@ -75,7 +75,7 @@
                     src="<?= base_url() ?>/assets/images/iconsEmail/linkedin.png" style="height:45px; width:45px; margin-left: -4px;"></a>
         </p>
         <p
-            style="Margin-top: 40px;color: #565656;font-size: 18px;line-height: 25px;Margin-bottom: 25px">
+            style="margin-left: 110px; width:80%; Margin-top: 40px;color: #565656;font-size: 18px;line-height: 25px;Margin-bottom: 25px">
             Disclimer : Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of
             classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor
             at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a
@@ -83,7 +83,7 @@
             undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum"
             (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of
             ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..",
-            comes from a line in section 1.10.32.</p>
+            comes from a line in section 1.10.32.</p> </div>
 
 
         <p align="center"
