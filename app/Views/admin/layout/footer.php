cf131ba06041aa7446f507a66888a0bbdb8e576d
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-16 17:36:34 +0530

    Spring And Fall Nazim 16 Dec

--- app/Views/admin/layout/footer.php
+++ app/Views/admin/layout/footer.php
@@ -1,7 +1,7 @@
  <!-- footer content -->
  <footer>
      <div class="pull-right">
-         Gentelella - Bootstrap Admin Template by <a href="https://colorlib.com">Colorlib</a>
+     DESIGNED AND DEVELOPED BY | © <a href="https://aarhatiserv.com/">AARHAT ISERV LLP</a>
      </div>
      <div class="clearfix"></div>
  </footer>
