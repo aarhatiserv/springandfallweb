d2ee30a7ea7d95d321d1d36774adc44b8314c180
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-16 17:50:35 +0530

    Spring And Fall Nazim 16 Dec 5

--- app/Views/admin/home.php
+++ app/Views/admin/home.php
@@ -12,7 +12,7 @@
     <title>Spring & Fall | Admin</title>
 
 <style>
-    .someclass {min-height: 200px !important;}
+    .someclassMax {max-height: 500px !important;}
 </style>
 </head>
 
@@ -29,7 +29,7 @@
 
 
             <!-- page content -->
-            <div class="right_col someclass" role="main">
+            <div class="right_col someclassMax" role="main">
                 <!-- top tiles -->
                 <div class="row tile_count">
                     <div class="col-md-2 col-sm-4 col-xs-6 tile_stats_count">
