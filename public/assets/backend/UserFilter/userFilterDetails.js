41f25021d5a27017599ee0c06cc6f08f0b1889ea
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-17 11:17:37 +0530

    Spring And Fall Nazim 16 Dec 2

--- public/assets/backend/UserFilter/userFilterDetails.js
+++ public/assets/backend/UserFilter/userFilterDetails.js
@@ -4,46 +4,48 @@ function getval() {
     var filter = document.getElementById("filterText").value;
     
     if (filter === today()) {
-        alert(today());
-    }
-    // console.log(filter);
-    let formData = new FormData();
-    formData.append('filter', filter);
-    $.ajax({
-        url: "/admin/PreviousData",
-        method: "POST",
-        data: formData,
-        processData: false,
-        contentType: false,
+        console.log('today', today());
+        console.log('filter', filter);
+    } else {
+        // console.log(filter);
+        let formData = new FormData();
+        formData.append('filter', filter);
+        $.ajax({
+            url: "/admin/PreviousData",
+            method: "POST",
+            data: formData,
+            processData: false,
+            contentType: false,
 
-        success: function(data) {
-            var res = JSON.parse(data);
-            let dt = [];
-            // $('#myTable').hide();
-            if (res.status !== undefined) {
+            success: function (data) {
+                var res = JSON.parse(data);
+                let dt = [];
+                // $('#myTable').hide();
+                if (res.status !== undefined) {
 
-                if (res.data.length == 0) {
-                    $('#myTable').html("No Record Found");
+                    if (res.data.length == 0) {
+                        $('#myTable').html("No Record Found");
 
-                } else {
-                    res.data.map((item) => (
-                        dt.push(
-                            ` <tr>
+                    } else {
+                        res.data.map((item) => (
+                            dt.push(
+                                ` <tr>
                                                 <td>` + item.name + `</td>
                                                 <td>` + item.email + `</td>
 
                                                 <td>` + item.phone + `</td>
                                                 <td>` + item.registered + `</td>
                                             </tr>`
-                        )
-                    ));
-                    // $("#listColleges").html(dt);
-                    $('#myTable').html(dt);
+                            )
+                        ));
+                        // $("#listColleges").html(dt);
+                        $('#myTable').html(dt);
 
+                    }
                 }
-            }
-        },
-    });
+            },
+        });
+    }
 
 }
 
