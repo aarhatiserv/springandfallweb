f1c6d7bfa75cef03fe1211938f4e6b993245f7ad
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-20 17:05:45 +0530

    Spring And Fall Nazim 21 Dec 3

--- app/Controllers/CollegeController.php
+++ app/Controllers/CollegeController.php
@@ -33,17 +33,18 @@ class CollegeController extends Controller
         $model = new CollegeModel();
 
         // $data = $model->where('country', $country)->findAll();
-        $data = [$country, $courses];
+        // $data = [$country, $courses];
         $dataAll = $model->where("country = '$country'")->findAll();
-        if(!empty($dataAll)){
-            for($i=0;$i<$dataAll.length;$i++){
-                $dataSeparate = explode (",", $dataAll[$i]['courses']);
-                if( $dataSeparate[$i] === $courses){
-                    $data = $model->where("country'$country'AND courses ='$courses' ")->findAll();
-                }
-        }
-    }
-        echo json_encode(["status" => 1, "data" => $data]);
+    //     if(!empty($dataAll)){
+    //         for($i=0;$i<$dataAll.length;$i++){
+    //             $dataSeparate = explode (",", $dataAll[$i]['courses']);
+    //             if( $dataSeparate[$i] === $courses){
+    //                 $data = $model->where("country'$country'AND courses ='$courses' ")->findAll();
+    //             }
+    //     }
+    // }
+    print_r($dataAll);
+        echo json_encode(["status" => 1, "data" => $dataAll]);
     }
     // ------------------------------------------Single Course-------------------------------
     public function getCourse($course)
