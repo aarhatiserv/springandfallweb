9a9c73a60039abca909860f3f71111be6b991a67
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-23 17:48:35 +0530

    Spring And Fall Nazim 23 Dec 9

--- app/Views/pages/newPassword.php
+++ app/Views/pages/newPassword.php
@@ -9,12 +9,52 @@ if ($session->get('token')) : ?>
     <!-- <button class="my-5" id="loginBtn" data-toggle="modal" data-target="#loginForm">SIGN IN</button> -->
     <?php
 if(!isset($_COOKIE[$LastID])) { ?>
-    <form action="setNewPassword" method="POST">
+    
         <input type="hidden" name="userID" id="userID" value="<?= $LastID ?>" >
     <input type="text" name="newPassword" id="newPassword" placeholder="New Password">
-    <input type="submit">
-</form>
+    <input type="submit" id="submit">
+
 <?php } ?>
 </div>
 
 <?php endif; ?>
+
+<script>
+ var userID = document.getElementById("userID");
+   var newPassword = document.getElementById("newPassword");
+   var btn = document.getElementById("submit");
+    btn.onclick = function () {
+        let formData = new FormData();
+    formData.append('userID', userID);
+    formData.append('newPassword', newPassword);
+    $.ajax({
+      url: "/setNewPassword",
+      method: "POST",
+      data: formData,
+      processData: false,
+      contentType: false,
+      beforeSend: function () {
+        $("#submit").val("Please wait");
+        $("#submit").prop("disabled", true);
+      },
+      success: function (data) {
+        $("#submit").prop("disabled", false);
+        var res = JSON.parse(data);
+          if (res.status === 1) {
+              swal({ title:"Thank you!", text:res.message, type:"success"} function () {
+                    $('#loginForm').modal('show');
+                  });
+          } else if (res.status === 2) {
+            
+              swal({ title: "Opps.!!", text: res.message, type: "error" });
+              
+        }else if (res.status === 3) {  
+            swal({ title: "Opps.!!", text: res.message, type: "error" });
+          
+      } else {
+            swal({ title: "Opps.!!", text: res.message, type: "error" });
+        }
+      }
+    })
+    }
+</script>
