592f99f92cb2fbdcd0ddb9e50f2ad31038befe99
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-23 12:29:57 +0530

    Spring And Fall Nazim 23 Dec 7

--- public/assets/js/profile.js
+++ public/assets/js/profile.js
@@ -1,11 +1,13 @@
-const personalDetails = document.getElementById("personalDetails");
-const changePassword = document.getElementById("changePassword");
-const btn = document.getElementById("changePasswordBtn");
-btn.onclick = function () {
-  if (personalDetails.style.display !== "none") {
-        personalDetails.style.display = "none";
-        changePassword.style.display = "block";
-  } else {
-    changePassword.style.display = "block";
-  }
-};
+$(document).ready(function () {
+    const personalDetails = document.getElementById("personalDetails");
+    const changePassword = document.getElementById("changePassword");
+    const btn = document.getElementById("changePasswordBtn");
+    btn.onclick = function () {
+        if (personalDetails.style.display !== "none") {
+            personalDetails.style.display = "none";
+            changePassword.style.display = "block";
+        } else {
+            changePassword.style.display = "block";
+        }
+    }
+});
