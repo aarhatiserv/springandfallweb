09e80dd8061991d93572753ddbe3724e37a90470
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-23 11:01:19 +0530

    Spring And Fall Nazim 23 Dec

--- app/Views/templates/header.php
+++ app/Views/templates/header.php
@@ -357,9 +357,28 @@ $session = session();
                         </li> -->
                         <li class="nav-item ml-2 mt-1">
                             <?php if ($session->get('token')) : ?>
-                            <a href="<?= base_url() ?>/api/logout" class="btn"
+                            <!-- <a href="" class="btn"
                                 style="color:white !important; line-height: 17px;" id="loginBtn" rel="noopener">SIGN
-                                OUT</a>
+                                OUT</a> -->
+
+                                <!-- profile drop down -->
+                                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbar-list-4" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+    <span class="navbar-toggler-icon"></span>
+  </button>
+  <div class="collapse navbar-collapse" id="navbar-list-4">
+    <ul class="navbar-nav">
+        <li class="nav-item dropdown">
+        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+          <img src="https://s3.eu-central-1.amazonaws.com/bootstrapbaymisc/blog/24_days_bootstrap/fox.jpg" width="40" height="40" class="rounded-circle">
+        </a>
+        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+          <a class="dropdown-item" href="#">Dashboard</a>
+          <a class="dropdown-item" href="#">Edit Profile</a>
+          <a class="dropdown-item" href="#">Log Out</a>
+        </div>
+      </li>   
+    </ul>
+  </div>
 
                             <?php else : ?>
                             <button id="loginBtn" data-toggle="modal" data-target="#loginForm">SIGN IN</button>
