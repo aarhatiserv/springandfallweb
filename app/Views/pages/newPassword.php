8163c7f95d8f2892db783dea5089d83e94f068e6
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-24 13:08:57 +0530

    Spring And Fall Nazim 24 Dec 5

--- app/Views/pages/newPassword.php
+++ app/Views/pages/newPassword.php
@@ -1,13 +1,16 @@
 <?php $session = session();
 if ($session->get('token')) : ?>
 
-<h1>Welcome to Spring and fall</h1>
+   <script>
+    window.location.href="https://springandfall.in/";
+    </script>
+
 
 <?php else : ?>
 <div class="jumbotron text-center">
     <h1 class="font-weight-light">New Password</h1>
     <!-- <button class="my-5" id="loginBtn" data-toggle="modal" data-target="#loginForm">SIGN IN</button> -->
-    <input type="text" name="newPassword" id="newPassword" placeholder="New Password">
+    <input type="password" name="newPassword" id="newPassword" placeholder="New Password">
     <button class="btn btn-primary font-weight-bold mb-2 mr-2" name="pass" id="pass" type="submit"
                         >Submit</button>
 </div>
