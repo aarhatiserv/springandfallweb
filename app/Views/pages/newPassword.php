30a11ec8c8f713c26033bb7c7485f6b23ea31985
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-24 11:26:08 +0530

    Spring And Fall Nazim 24 Dec 9

--- app/Views/pages/newPassword.php
+++ app/Views/pages/newPassword.php
@@ -9,7 +9,7 @@ if ($session->get('token')) : ?>
     <!-- <button class="my-5" id="loginBtn" data-toggle="modal" data-target="#loginForm">SIGN IN</button> -->
     <input type="text" name="newPassword" id="newPassword" placeholder="New Password">
     <button class="btn btn-primary font-weight-bold mb-2 mr-2" name="pass" id="pass" type="submit"
-                        disabled="disabled">Submit</button>
+                        >Submit</button>
 </div>
 
 <?php endif; ?>
