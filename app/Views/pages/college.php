fb2e108aa1210ed2536853596d56f0dd8593b65d
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-28 12:05:54 +0530

    Spring And Fall Uploaded By Nazim 28 Dec 3

--- app/Views/pages/college.php
+++ app/Views/pages/college.php
@@ -1,7 +1,7 @@
 <div class="container-fluid" style="margin-bottom: 15px; margin-left: 35px;">
     <div class="row">
         <div class="col-1">
-            <a class="text-dark" style="" onclick="window.history.go(-1)"><i class="fas fa-less-than"></i><b> Back</b></a>
+            <a class="text-dark" href="" onclick="window.history.go(-1)"><i class="fas fa-less-than"></i><b> Back</b></a>
         </div>
         <div class="col-11"></div>
     </div>
