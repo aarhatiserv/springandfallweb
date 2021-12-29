bd908ea19e348c5b30551018ae82ae0b7fe38427
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-29 17:10:09 +0530

    Spring And Fall Uploaded By Nazim 29 Dec 8

--- app/Views/admin/Course/AddCourse.php
+++ app/Views/admin/Course/AddCourse.php
@@ -10,14 +10,41 @@
     <link rel="icon" href="../fav.ico" type="image/ico">
     <title>Spring & Fall | Admin</title>
 
+    
+    <!-- Bootstrap -->
+    <link href="../../assets/backend/vendors/bootstrap/dist/css/bootstrap.min.css" rel="stylesheet">
+    <!-- Font Awesome -->
+    <link href="../../assets/backend/vendors/font-awesome/css/font-awesome.min.css" rel="stylesheet">
+    <!-- NProgress -->
+    <link href="../../assets/backend/vendors/nprogress/nprogress.css" rel="stylesheet">
+    <!-- iCheck -->
+    <link href="../../assets/backend/vendors/iCheck/skins/flat/green.css" rel="stylesheet">
+    <!-- Datatables -->
+    <link href="../../assets/backend/vendors/datatables.net-bs/css/dataTables.bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/backend/vendors/datatables.net-buttons-bs/css/buttons.bootstrap.min.css" rel="stylesheet">
+    <link href="../../assets/backend/vendors/datatables.net-fixedheader-bs/css/fixedHeader.bootstrap.min.css"
+        rel="stylesheet">
+    <link href="../../assets/backend/vendors/datatables.net-responsive-bs/css/responsive.bootstrap.min.css"
+        rel="stylesheet">
+    <link href="../../assets/backend/vendors/datatables.net-scroller-bs/css/scroller.bootstrap.min.css"
+        rel="stylesheet">
+    <!-- bootstrap-progressbar -->
+    <link href="../../assets/backend/vendors/bootstrap-progressbar/css/bootstrap-progressbar-3.3.4.min.css"
+        rel="stylesheet">
+    <!-- JQVMap -->
+    <link href="../../assets/backend/vendors/jqvmap/dist/jqvmap.min.css" rel="stylesheet" />
+    <!-- bootstrap-daterangepicker -->
+    <link href="../../assets/backend/vendors/bootstrap-daterangepicker/daterangepicker.css" rel="stylesheet">
+
+    <!-- Custom Theme Style -->
+    <link href="../../assets/backend/build/css/custom.min.css" rel="stylesheet">
+    <link href="../../assets/backend/build/css/addColleges.css" rel="stylesheet">
+
     <style>
     .someclassMax {
         min-height: 670px !important;
     }
     </style>
-
-<?= $this->include('admin/layout/stylesheet') ?>
-<?= $this->include('admin/layout/script') ?>
 </head>
 
 <body class="nav-md">
@@ -113,5 +140,69 @@
 
 
 </body>
-
+<!-- jQuery -->
+<script src="../../assets/backend/vendors/jquery/dist/jquery.min.js"></script>
+<!-- Bootstrap -->
+<script src="../../assets/backend/vendors/bootstrap/dist/js/bootstrap.min.js"></script>
+<!-- FastClick -->
+<script src="../../assets/backend/vendors/fastclick/lib/fastclick.js"></script>
+<!-- NProgress /..-->
+<script src="../../assets/backend/vendors/nprogress/nprogress.js"></script>
+<!-- Chart.js -/..->
+<script src="../../assets/backend/vendors/Chart.js/dist/Chart.min.js"></script>
+<!-- gauge.js -/..->
+<script src="../../assets/backend/vendors/gauge.js/dist/gauge.min.js"></script>
+<!-- bootstrap-/..progressbar -->
+<script src="../../assets/backend/vendors/bootstrap-progressbar/bootstrap-progressbar.min.js"></script>
+<!-- iCheck -->/..
+<script src="../../assets/backend/vendors/iCheck/icheck.min.js"></script>
+<!-- Skycons --/..>
+<script src="../../assets/backend/vendors/skycons/skycons.js"></script>
+<!-- Flot -->/..
+<script src="../../assets/backend/vendors/Flot/jquery.flot.js"></script>
+<script src="../../assets/backend/vendors/Flot/jquery.flot.pie.js"></script>
+<script src="../../assets/backend/vendors/Flot/jquery.flot.time.js"></script>
+<script src="../../assets/backend/vendors/Flot/jquery.flot.stack.js"></script>
+<script src="../../assets/backend/vendors/Flot/jquery.flot.resize.js"></script>
+<!-- Flot plugi/..ns -->
+<script src="../../assets/backend/vendors/flot.orderbars/js/jquery.flot.orderBars.js"></script>
+<script src="../../assets/backend/vendors/flot-spline/js/jquery.flot.spline.min.js"></script>
+<script src="../../assets/backend/vendors/flot.curvedlines/curvedLines.js"></script>
+<!-- DateJS -->/..
+<script src="../../assets/backend/vendors/DateJS/build/date.js"></script>
+<!-- JQVMap -->/..
+<script src="../../assets/backend/vendors/jqvmap/dist/jquery.vmap.js"></script>
+<script src="../../assets/backend/vendors/jqvmap/dist/maps/jquery.vmap.world.js"></script>
+<script src="../../assets/backend/vendors/jqvmap/examples/js/jquery.vmap.sampledata.js"></script>
+<!-- bootstrap-/..daterangepicker -->
+<script src="../../assets/backend/vendors/moment/min/moment.min.js"></script>
+<script src="../../assets/backend/vendors/bootstrap-daterangepicker/daterangepicker.js"></script>
+/..
+<!-- Custom The/..me Scripts -->
+<script src="../../assets/backend/build/js/custom.min.js"></script>
+<!-- jQuery Spa/..rklines -->
+<script src="../../assets/backend/vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+/..
+<!-- jQuery Spa/..rklines -->
+<script src="../../assets/backend/vendors/jquery-sparkline/dist/jquery.sparkline.min.js"></script>
+<!-- morris.js /..-->
+<script src="../../assets/backend/vendors/raphael/raphael.min.js"></script>
+<script src="../../assets/backend/vendors/morris.js/morris.min.js"></script>
+/..
+<!-- Datatables/.. -->
+<script src="../../assets/backend/vendors/datatables.net/js/jquery.dataTables.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-bs/js/dataTables.bootstrap.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-buttons/js/dataTables.buttons.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-buttons-bs/js/buttons.bootstrap.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-buttons/js/buttons.flash.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-buttons/js/buttons.html5.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-buttons/js/buttons.print.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-fixedheader/js/dataTables.fixedHeader.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-keytable/js/dataTables.keyTable.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-responsive/js/dataTables.responsive.min.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-responsive-bs/js/responsive.bootstrap.js"></script>
+<script src="../../assets/backend/vendors/datatables.net-scroller/js/dataTables.scroller.min.js"></script>
+<script src="../../assets/backend/vendors/jszip/dist/jszip.min.js"></script>
+<script src="../../assets/backend/vendors/pdfmake/build/pdfmake.min.js"></script>
+<script src="../../assets/backend/vendors/pdfmake/build/vfs_fonts.js"></script>
 </html>
