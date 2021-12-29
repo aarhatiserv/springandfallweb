888821fda791d3ea80bbf37c2d80b56237e2a112
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-29 12:55:11 +0530

    Spring And Fall Uploaded By Nazim 29 Dec 5

---
+++ app/Views/admin/Courses/AddCourse.php
@@ -0,0 +1,88 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <!-- Meta, title, CSS, favicons, etc. -->
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" href="../fav.ico" type="image/ico">
+    <title>Spring & Fall | Admin</title>
+
+    <style>
+    .someclassMax {
+        min-height: 670px !important;
+        }
+</style>
+</head>
+
+<body class="nav-md">
+    <div class="container body">
+        <div class="main_container">
+            <!-- ------------------------------------------Start Left Nav------------------------ -->
+            <?=$this->include('admin/layout/left_nav')?>
+            <!-- ------------------------------------------Start Left Nav------------------------ -->
+
+            <!-- ------------------------------------------Start header------------------------ -->
+            <?=$this->include('admin/layout/header')?>
+            <!-- ------------------------------------------End header-------------------------- -->
+
+
+            <!-- page content -->
+            <div class="right_col someclassMax" role="main">
+                <div class="">
+                    <div class="page-title">
+                        <div class="title_left">
+                            <h3>Courses
+                            </h3>
+                        </div>
+                    </div>
+
+                    <div class="clearfix"></div>
+
+                    <div class="row">
+                        <div class="col-md-12 col-sm-12 col-xs-12">
+                            <div class="x_panel">
+                                <div class="x_title">
+                                    <h2>Add Course
+                                    </h2>
+
+                                    <div class="clearfix"></div>
+                                </div>
+                                <div class="x_content">
+                                    <form action="/admin/addPages" method="POST">
+                                        <div>
+
+                                            <label for="name">Course Name</label>
+                                           
+                                            <br>
+
+                                            <input type="text" id="name" name="name"
+                                                placeholder="Course Name..."
+                                                required />
+                                        </div>
+                                        <br>
+
+                                        <button class="back"><a href="/admin/courses" class="back">Back</a>
+                                        </button>
+                                        <input type="submit" value="Submit">
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+            </div>
+            <!-- /page content -->
+            <!-- ------------------------------------------Start Footer------------------------ -->
+            <?=$this->include('admin/layout/footer')?>
+            <!-- ------------------------------------------Start Footer------------------------ -->
+
+        </div>
+    </div>
+
+
+</body>
+</html>
