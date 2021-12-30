13b64264e36f439b8cdccd58990f81a8c76e3888
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-30 16:08:03 +0530

    Spring and Fall uploaded by Nazi 30 Dec 1

--- app/Controllers/CollegeController.php
+++ app/Controllers/CollegeController.php
@@ -641,14 +641,12 @@ public function getCoursesClickCountry($country){
         $cmodel = new CourseModel();
         $dataCourse = $cmodel->findAll();
     
-        $data['collegeDetails'] = array();
-        for($i = 0; $i<count($dataCourse); $i++){
+
             // $data.push($data2[$i]);
             $db = \Config\Database::connect();
             $query = $db->query("SELECT DISTINCT  department.id as d_id, department.name as d_name, colleges.id as college_id, colleges.names as college_name, colleges.country as country, colleges.image as image, colleges.image_background as image_background, colleges.about as about, level.id as l_id, level.name as l_name FROM course INNER JOIN department ON course.department_id = department.id INNER JOIN colleges ON course.college_id = colleges.id INNER JOIN level ON course.level_id = level.id WHERE course.college_id = '".$dataCollege[$i]['id']."'");
-            array_push($data['collegeDetails'], $query->getResult()); 
-        }
-
+            $data['collegeDetails'] =  $query->getResult(); 
+    
 
         //  print_r( $data[ 'collegeDetails' ][ 0 ][ 'country' ] );
         // $data[ 'similarUniversity' ] = $model->where( 'country = ', $data[ 'collegeDetails' ][ 0 ][ 'country' ] )->findAll( 4 );
