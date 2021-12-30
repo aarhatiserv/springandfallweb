f259aed1028884ceb4f3f3feacd50edf636ad9d9
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-30 17:25:14 +0530

    Spring and Fall uploaded by Nazi 30 Dec 2

--- app/Views/pages/career-guide.php
+++ app/Views/pages/career-guide.php
@@ -327,7 +327,7 @@
                         <label class="form-check-label" for="level">Diploma</label>
                     </div>
                     <div class="form-group mx-2">
-                        <input class="form-check-input" type="radio" name="level" id="level" value="ExecutiveCourse">
+                        <input class="form-check-input" type="radio" name="level" id="level" value="Executivecourse">
                         <label class="form-check-label" for="level">Executive Course</label>
                     </div>
                 </div>
