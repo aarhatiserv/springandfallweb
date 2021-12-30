e79921048b6c20e15a22aafbf79af831e8360b29
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-30 14:38:34 +0530

    Spring and Fall uploaded by Nazi 30 Dec 3

--- public/assets/js/career-guide.js
+++ public/assets/js/career-guide.js
@@ -287,25 +287,26 @@ function getCollegeInCareer(country) {
           } else {
             collegeData.data.map((item2) => item2.map((item) => (
               dt.push(
-                ` <li class="media my-4 bg-light">
-                    <img class="p-3 image" style="max-width: 20%; height: 167px;" src="uploads/CollegesImage/` + item.country + `/logo/` + item.image +`" class="mr-3" alt="..." title="hrl" width="" height="" />
-                      <div class="media-body py-3">
-                        <div class="row">
-                          <div class="col-md-8">
-                          <a href="/college/` + item.country + `/` + item.id + `"><p class="mt-0 mb-1 media-heading pb-2 h5_2_P">` + item.names +`</p></a>
-                              <p>` + item.country +` </p> 
-                              <p>` + item.courses.replace(/,/g, ', ') +` </p>
+                `  <li class="media my-4 bg-light">
+                <img class="p-3 image" style="max-width: 20%; height: 167px;" src="uploads/CollegesImage/` + item.country + `/logo/` + item.image + `" class="mr-3" alt="..."
+                    title="hrl" width="" height="" />
+                <div class="media-body py-3">
+                    <div class="row">
+                        <div class="col-md-8">
+                        <a href="/college/` + item.country + `/` + item.college_id + `"><p class="mt-0 mb-1 media-heading pb-2 h5_2_P">` + item.college_name + `</p></a>
+                            <p>` + item.country + ` </p>
+                            <p>` + item.d_name +` </p>
+                            
                           </div>
                         <div class="col-md-4 d-flex justify-content-end align-items-center ">
-                      <div class="apply px-5">
-                       
-                        <button class="btn applyNow" onclick="applyForCollages(` + item.id +`)">Apply now</button>
-                                                
-                      </div>
+                            <div class="apply px-5">
+                                
+                                <button class="btn applyNow" onclick="applyForCollegeInConsultation(` + item.id +`)">Apply now</button>
+                            </div>
+                        </div>
                     </div>
-                  </div>
                 </div>
-              </li>`
+            </li>`
               ))
             ));
             // $("#listColleges").html(dt);
