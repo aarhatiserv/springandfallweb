0b6ad1960d0a08642ff1c1f9834b30dabd212b35
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-30 10:45:50 +0530

    Spring and Fall uploaded by Nazi 30 Dec 4

--- app/Views/admin/Course/AddCourse.php
+++ app/Views/admin/Course/AddCourse.php
@@ -451,40 +451,6 @@
             </div>
             <!-- /page content -->
 
-            <!-- page content -->
-            <div class="right_col someclassMax" role="main">
-                <div class="">
-                    <div class="page-title">
-                        <div class="title_left">
-                            <h3>Course
-                                <!-- <small>Some examples to get you started</small> -->
-                            </h3>
-                        </div>
-                    </div>
-
-                    <div class="clearfix"></div>
-
-                    <div class="row">
-
-
-                        <div class="col-md-12 col-sm-12 col-xs-12">
-                            <div class="x_panel">
-                                <div class="x_title">
-                                    <h2>Course Data
-                                        <!-- <small>Users</small> -->
-                                    </h2>
-
-                                    <div class="clearfix"></div>
-                                </div>
-
-                            </div>
-                        </div>
-
-
-                    </div>
-                </div>
-            </div>
-            <!-- /page content -->
             <!-- ------------------------------------------Start Footer------------------------ -->
             <?= $this->include('admin/layout/footer')?>
             <!-- ------------------------------------------Start Footer------------------------ -->
