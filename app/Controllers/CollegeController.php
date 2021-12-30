8bf43ab1ec5ef61afd011a8e7d979150680897ff
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-30 13:03:30 +0530

    Spring and Fall uploaded by Nazi 30 Dec 5

--- app/Controllers/CollegeController.php
+++ app/Controllers/CollegeController.php
@@ -27,7 +27,7 @@ class CollegeController extends Controller
                 // $data.push($data2[$i]);
                 $db = \Config\Database::connect();
                 $query = $db->query("SELECT  department.name as d_name, colleges.id as college_id FROM course INNER JOIN department ON course.department_id = department.id INNER JOIN colleges ON course.college_id = colleges.id WHERE course.college_id = '".$data2[$i]['id']."'");
-                array_unique($query);
+                $query =  array_unique($query);
                 array_push($data, $query->getResult());
             }
             // $data = array_unique($data);
