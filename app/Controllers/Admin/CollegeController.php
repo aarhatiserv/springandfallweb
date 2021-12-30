ba93b91b8c6f3eff7ab79ab12041981e536db368
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-30 10:47:51 +0530

    Spring and Fall uploaded by Nazi 30 Dec 5

--- app/Controllers/Admin/CollegeController.php
+++ app/Controllers/Admin/CollegeController.php
@@ -364,11 +364,11 @@ class CollegeController extends Controller
             $data['allDepartmentData'] = $dmodel->findAll();
             $data['allLevelData'] = $lmodel->findAll();
 
-            // $db = \Config\Database::connect();
-            // $query = $db->query('SELECT department.name as d_name, level.name as l_name, name FROM course INNER JOIN department ON course.department_id = department.id INNER JOIN level ON course.level_id = level.id');
-            // $data['courseDetails'] = $query->getResult();
+            $db = \Config\Database::connect();
+            $query = $db->query("SELECT department.name as d_name, level.name as l_name, name FROM course INNER JOIN department ON course.department_id = department.id INNER JOIN level ON course.level_id = level.id WHERE course.college_id = '$id'");
+            $data['courseDetails'] = $query->getResult();
      
-            $data['courseDetails'] = $coursemodel->where('college_id', $id)->findAll();
+            // $data['courseDetails'] = $coursemodel->where('college_id', $id)->findAll();
 
             echo view('admin/Course/AddCourse', $data);
  
