2487c5e44d0a1a72da0dfd24ea540593bafa3b08
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-30 16:04:46 +0530

    Spring and Fall uploaded by Nazi 30 Dec 7

--- app/Controllers/CollegeController.php
+++ app/Controllers/CollegeController.php
@@ -645,7 +645,7 @@ public function getCoursesClickCountry($country){
         for($i = 0; $i<count($dataCourse); $i++){
             // $data.push($data2[$i]);
             $db = \Config\Database::connect();
-            $query = $db->query("SELECT DISTINCT  department.id as d_id, department.name as d_name, colleges.id as college_id, colleges.names as college_name, colleges.country as country, colleges.image as image, colleges.image_background as image_background, level.id as l_id, level.name as l_name FROM course INNER JOIN department ON course.department_id = department.id INNER JOIN colleges ON course.college_id = colleges.id INNER JOIN level ON course.level_id = level.id WHERE course.college_id = '".$dataCollege[$i]['id']."'");
+            $query = $db->query("SELECT DISTINCT  department.id as d_id, department.name as d_name, colleges.id as college_id, colleges.names as college_name, colleges.country as country, colleges.image as image, colleges.image_background as image_background, colleges.about as about, level.id as l_id, level.name as l_name FROM course INNER JOIN department ON course.department_id = department.id INNER JOIN colleges ON course.college_id = colleges.id INNER JOIN level ON course.level_id = level.id WHERE course.college_id = '".$dataCollege[$i]['id']."'");
             array_push($data['collegeDetails'], $query->getResult()); 
         }
 
