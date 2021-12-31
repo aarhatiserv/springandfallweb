af74a45eb3ca6eb98f12156b5b9edb6e096534c7
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2021-12-31 13:16:24 +0530

    Spring and Fall uploaded by Nazim 31 Dec 5

--- public/assets/js/college.js
+++ public/assets/js/college.js
@@ -17,3 +17,23 @@ $(".open").click(function () {
     container.addClass("expanded");
   }
 });
+
+$(".open2").click(function () {
+  var container = $(this).parents(".topic2");
+  var answer = container.find(".answer2");
+  var trigger = container.find(".faq-t2");
+
+  answer.slideToggle(200);
+
+  if (trigger.hasClass("faq-o2")) {
+    trigger.removeClass("faq-o2");
+  } else {
+    trigger.addClass("faq-o2");
+  }
+
+  if (container.hasClass("expanded2")) {
+    container.removeClass("expanded2");
+  } else {
+    container.addClass("expanded2");
+  }
+});
