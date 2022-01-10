502fb370921523ecbcabddaeab3d25798f0fe0eb
Sk-Abdul-Nazim <sknazim1818@gmail.com>
2022-01-10 17:20:43 +0530

    Spring And Fall Uploaded By Nazim 10 Jan 1

--- app/Controllers/Admin/CollegeController.php
+++ app/Controllers/Admin/CollegeController.php
@@ -58,7 +58,7 @@ class CollegeController extends Controller
             "country" => "required",
             "address" => "required",
             "about" => "required",
-            "offers" => "required",
+            // "offers" => "required",
             // "course" => "required",
             // "level" => "required",
             // "file" => "required",
@@ -82,9 +82,9 @@ class CollegeController extends Controller
             "about" => [
                 "required" => "About is required",
             ],
-            "offers" => [
-                "required" => "Offers is required",
-            ],
+            // "offers" => [
+            //     "required" => "Offers is required",
+            // ],
             // "course" => [
             //     "required" => "Course is required",
             // ],
